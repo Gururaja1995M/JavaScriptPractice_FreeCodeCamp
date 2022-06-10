4f1578118d1882045f73c141ca73d0e7df3509c4
Gururaja1995M <gururajam1995@gmail.com>
2022-06-10 19:59:49 +0530

    Variables Learning

---
+++ FreeCodeCampLearning/DataTypes.js
@@ -0,0 +1,44 @@
+/* undefined, null, boolean, string, symbol, number and object */
+
+
+
+/*
+var myName="Guru"
+myName=8     // Can be accessed throghout the program
+
+
+let ourName="FreeCodeCamp" // Limited to declared scope
+
+const pi=3.14   // Variable constants cant be changed
+*/
+
+
+/*
+var a;
+var b=2;
+console.log(a);
+
+a=7;
+
+b=a;
+
+console.log(a);
+
+op:
+undefined
+7
+*/
+
+var a= 5;
+var b=10;
+var c="I am a";
+
+a=a+1;
+b=b+5;
+c=c+" String";
+console.log(a); //6
+console.log(b); //15
+console.log(c); // I am a String
+
+
+// 23 minutes over
