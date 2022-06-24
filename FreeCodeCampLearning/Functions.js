277fb8b20cbf4b6f7541df04f359daadce3898c6
Gururaja1995M <gururajam1995@gmail.com>
2022-06-24 19:26:43 +0530

    Functions Learning

--- FreeCodeCampLearning/Functions.js
+++ FreeCodeCampLearning/Functions.js
@@ -97,6 +97,7 @@ function processArg(num){
 processed = processArg(7);
 console.log(processed); // 2
 */
+/*
 //Stand in Line
 function nextLine(arr, item){
 arr.push(item);
@@ -109,3 +110,27 @@ var testArr= [1,2,3,4,5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextLine(testArr,6))
 console.log("After: " + JSON.stringify(testArr));
+
+//Boolean Values
+function welcomeToBooleans() {
+    return false;
+}
+*/
+/*
+// Returning Boolean Values from Function
+function isLess(a,b) {
+    
+        return a<b;
+    
+}
+console.log(isLess(20,15)); */
+
+// Returning Early Pattern from Functions
+function abTest(a,b) {
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b),2));
+}
+console.log(abTest(-1,3));
