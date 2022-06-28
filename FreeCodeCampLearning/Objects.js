07341736b1483f813fe532d3902cb963b257fccd
Gururaja1995M <gururajam1995@gmail.com>
2022-06-28 20:29:48 +0530

    Learning objects

---
+++ FreeCodeCampLearning/Objects.js
@@ -0,0 +1,56 @@
+
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+ourDog.name = "Happy Camper";
+console.log(ourDog.name);
+/*
+var myDog = {
+    "name": "Quincy",
+    "legs": 3,
+    "tails": 2,
+    "friends": []
+};
+
+// Accessing Object Properties with Dot Notation
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;
+console.log(hatValue);
+var shirtValue = testObj.shirt;
+console.log(shirtValue);
+
+// Accesing Object Properties with Bracket Notation
+var testObj2 = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+var entreeValue = testObj2["an entree"];
+var drinkValue = testObj2["the drink"];
+console.log(entreeValue + drinkValue);
+
+// Accessing Object Properties with Variables
+var testObj3 = {
+   12: "Namath",
+   16: "Montana",
+   19: "Unitas"
+};
+var playerNumber =16;
+var player = testObj3[playerNumber];
+console.log(player);
+*/
+
+// Updating Object Properties
+// follow 1 st example
+//ourDog.name = ""Happy Camper";
+
+//Add new properties to an object
+//1:54:30
