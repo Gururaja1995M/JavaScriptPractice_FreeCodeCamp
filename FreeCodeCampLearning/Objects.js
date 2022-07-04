3711bd335192413cd9de1d1901465ee4b8a321d7
Gururaja1995M <gururajam1995@gmail.com>
2022-07-04 20:34:12 +0530

    Obejects Cont..

--- FreeCodeCampLearning/Objects.js
+++ FreeCodeCampLearning/Objects.js
@@ -1,4 +1,4 @@
-
+/*
 var ourDog = {
     "name": "Camper",
     "legs": 4,
@@ -7,6 +7,7 @@ var ourDog = {
 };
 ourDog.name = "Happy Camper";
 console.log(ourDog.name);
+*/
 /*
 var myDog = {
     "name": "Quincy",
@@ -114,7 +115,7 @@ return result;
 }
 console.log(phoneticLookup2("echo"));
 */
-
+/*
 // Testing Objects for properties
 var myObj = {
     "gift": "pony",
@@ -131,3 +132,84 @@ function checkObj(checkProp) {
 
 }
 console.log(checkObj("bed"));
+*/
+
+//Manipulating Complex Objects
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Beau Carnes",
+        "title": "Cereal Man",
+        "release_year": 2003,
+        "formats": [
+            "YouTube video"
+        ]
+    }   
+]
+console.log(myMusic[0].formats[1]);
+
+// Accessing Nested Objects
+var myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passanger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+var gloveBoxContents = myStorage.car.inside["glove box"];
+console.log(gloveBoxContents);
+
+// Record Collection
+var collection = {
+    "2548": {
+        "album": "Slippery when wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let it Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2568": {
+        "album": "1999",
+        "artist": "Prience",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    }
+}
+
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value) {
+    if (value === "") {
+        delete collection[id][prop];
+
+    }else if (prop === "tracks") {
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value);
+    } else {
+        collection[id][prop] = value;
+    }
+
+    return collection;
+}
+updateRecords(2568, "tracks", "test");
+console.log(updateRecords(2568, "artist", "ABBA"));
+
+// Iterate with while loops
+//2:10:15
